8605acd2a13370063c9d0823f61c022b070491d4
Anca Simon <ancarsimon@gmail.com>
2020-06-01 19:51:58 -0500

    got auth working

--- src/components/shared/MyNavbar/MyNavbar.js
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import './MyNavbar.scss';
 
 class MyNavbar extends React.Component {
+  logoutEvent = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  }
+
   render() {
     return (
       <div className="MyNavbar">
         <h1>My Navbar</h1>
+        <button className="btn btn-secondary" onClick={this.logoutEvent}>Log Out</button>
       </div>
     );
   }
