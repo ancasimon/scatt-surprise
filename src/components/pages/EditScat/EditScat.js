9e0df8be3275d0711d28ceee3cd463f3087865cb
Anca Simon <ancarsimon@gmail.com>
2020-06-01 21:29:16 -0500

    wip

--- src/components/pages/EditScat/EditScat.js
+++ src/components/pages/EditScat/EditScat.js
@@ -4,9 +4,11 @@ import './EditScat.scss';
 
 class EditScat extends React.Component {
   render() {
+    const editId = this.props.match.params.scatId;
     return (
       <div className="EditScat">
         <h1>Edit Scat Component</h1>
+        <h2>The Scat Id is: {editId}</h2>
       </div>
     );
   }
