981aa2c30e6089d1a37112a8b95694331d273bc6
Anca Simon <ancarsimon@gmail.com>
2020-06-06 10:51:05 -0500

    displaying scat data on Home

---
+++ src/components/shared/ScatCard/ScatCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './ScatCard.scss';
+
+class ScatCard extends React.Component {
+  render() {
+    const { scat } = this.props;
+
+    return (
+      <div className="ScatCard">
+        {scat.location}
+      </div>
+    );
+  }
+}
+
+export default ScatCard;
